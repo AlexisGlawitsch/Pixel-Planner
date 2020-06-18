810eb21a9f595b870738bd995051ff9a776096bf
AlexisGlawitsch <lexiglawitsch@gmail.com>
2020-06-17 19:21:28 -0700

    Added basic grid functionality. Can paint on the grid by clicking and dragging, switch colors, and reset the grid.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Grid from "./components/grid.jsx";
+import ColorPicker from "./components/color_picker.jsx";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.onColorClick = this.onColorClick.bind(this);
+
+    this.state = {
+      currentColor: "#ff4d4d",
+      gridKey: 0,
+    };
+  }
+
+  onColorClick(color) {
+    console.log("Changed selected color to " + color);
+    this.setState({ currentColor: color });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="d-flex justify-content-around align-items-center top-bar">
+          <h1 className="inline">Pixel Planner</h1>
+          <div className="dropdown inline">
+            <button
+              className="btn btn-secondary dropdown-toggle inline"
+              type="button"
+              id="dropdownMenuButton"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Load
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="#">
+                Action
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-sm-8">
+            <div className="grid">
+              <Grid
+                currentColor={this.state.currentColor}
+                key={this.state.gridKey}
+              />
+            </div>
+          </div>
+          <div className="col-sm-4">
+            <p>Pick a color</p>
+            <ColorPicker onClick={this.onColorClick} />
+
+            <div className="shape-box">
+              <p className="no-margin">Generate shapes</p>
+              <p class="small-text">Click and drag</p>
+
+              <div className="inline">
+                <button className="btn btn-light">Rectangle</button>
+                <button className="btn btn-light">Circle</button>
+              </div>
+            </div>
+
+            <button
+              className="btn btn-light"
+              onClick={() => {
+                if (this.state.gridKey === 0) {
+                  this.setState({ gridKey: 1 });
+                } else {
+                  this.setState({ gridKey: 0 });
+                }
+              }}
+            >
+              Reset
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
